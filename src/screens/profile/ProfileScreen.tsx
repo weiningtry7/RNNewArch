0a9295ceb6caeef475586f9bc3c8eb9aaa70547c
伟宁 <1131811951@qq.com>
2025-12-05 13:45:06 +0800

    测试图片选择器依赖库

--- src/screens/profile/ProfileScreen.tsx
+++ src/screens/profile/ProfileScreen.tsx
@@ -2,8 +2,12 @@ import { useState } from 'react';
 import {
     View,
     Text,
-    Button
+    Button,
+    Platform,
+    PermissionsAndroid
 } from 'react-native'
+
+import HTImagePicker from '@hotta/react-native-imagepicker';
 
 function Header() {
   console.log('------Header rendered------');
@@ -12,11 +16,41 @@ function Header() {
 function TestReactComplier() {
   const [count, setCount] = useState(0);
   console.log('--------TestReactComplier rendered-------');
+  const _onPress = async () => {
+    const options: any = {
+      mediaType: 'photo',
+      sortOrder: 'asc',
+    }
+    // 添加权限检查（Android 13+ 需要 READ_MEDIA_IMAGES）
+    if (Platform.OS === 'android') {
+      let permission = PermissionsAndroid.PERMISSIONS.READ_EXTERNAL_STORAGE;
+      if (Platform.Version >= 33) { // Android 13+
+        permission = PermissionsAndroid.PERMISSIONS.READ_MEDIA_IMAGES;
+      }
+        const granted = await PermissionsAndroid.request(
+          permission,
+        {
+          title: '允许"App"访问照片、视频?',
+          message: '访问你的照片、视频、音频等媒体内容，以编辑或管理媒体内容',
+          buttonNeutral: '稍后再说',
+          buttonNegative: '取消',
+          buttonPositive: '确定',
+        }
+      );
+      if (granted !== PermissionsAndroid.RESULTS.GRANTED) {
+        throw new Error('没有访问相册权限');
+      }
+    }
+    const response = await HTImagePicker.asyncShowImagePicker(options);
+    console.log('------==asyncShowImagePicker------')
+    console.log(response)
+  }
+
   return (
     <View style={{}}>
       <Header />  
       <Text>Count: {count}</Text>
-      <Button title="Increment" onPress={() => setCount(c => c + 1)} />
+      <Button title="Increment" onPress={_onPress} />
     </View>
   );
 }
